59b399c9d36502912cf2c048c9e75d8fd57069fe
praxter11 <parveshsaini619@gmail.com>
2023-03-19 15:48:36 +0530

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
+import {Routes, Route} from 'react-router-dom';
 
+import Navigation from './routes/navigation/navigation.component';
+import Home from "./routes/home/home.component";
 
-import Directory from "./components/directory/directory.component";
+const Shop= ()=>{
+  return(
+    <h1>waddup, im shop</h1>
+  )
+}
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
 
-  return (
+  return(
 
-    <Directory categories={categories} />
-    
-  );
+    <Routes>
+      {/* Parent route always persisits on page, childs render according to paths */}
+      <Route path='/' element={<Navigation />} >
+
+         <Route index={true} element={<Home/>} /> {/*Now Home component will also be shown along with navigation upon land '/' */}
+
+         <Route path='shop' element={<Shop/>} />
+
+      </Route>
+      
+      
+    </Routes>
+  ) 
 };
 
 export default App;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,32 @@
+import { Outlet } from 'react-router-dom'; //allows nesting routes to render acccording to selected pattern
+import { Fragment } from 'react'; //useful if we dont wish to render some html element
+import { Link } from 'react-router-dom'; //anchor tags in react
+import { ReactComponent as EliteLogo} from '../../assets/elite.svg'
+
+import './navigation.styles.scss'
+
+const Navigation = ()=>{
+    return (
+      <Fragment>
+        <div className='navigation'>
+
+          <Link className='logo-container' to='/'>
+            <EliteLogo className='logo' />
+          </Link>
+          
+          <div className="nav-links-container">
+            <Link className='nav-link' to='/shop'>
+              <div>Shop</div>
+            </Link>
+
+          </div>
+      
+        </div>
+  
+         <Outlet />  {/* Nav bar is persisitent throughout, while other components will come under navbar bcz outlet is placed under navbar */}
+  
+      </Fragment> //I dont need a wrapping div in this component, hence replace it with <Fragment></Fragment>
+    )
+  }
+
+  export default Navigation
